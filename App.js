6a1ffa192d61af93db17ab113e409d9dd584ffc7
Monica <monica.walentek@gmail.com>
2022-05-16 10:16:23 +0200

    initializing project

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './App/screens/HomeScreen';
+import LoginScreen from './App/screens/LoginScreen';
+import RegisterScreen from './App/screens/RegisterScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Token } from './App/context/context';
+import { useState } from 'react';
+import React from 'react';
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+
+export default function App()  {
+
+  const [token, setToken] = useState([]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Teams.Provider value={{token, setToken}}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+      </Teams.Provider>
+    </NavigationContainer>
+
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
