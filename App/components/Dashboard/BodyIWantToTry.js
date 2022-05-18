444b35a5b9e9b56f29418d62f6bffc62e54eaf14
Monica <monica.walentek@gmail.com>
2022-05-18 12:16:43 +0200

    handle edit on I Want To Try Body

--- App/components/Dashboard/BodyIWantToTry.js
+++ App/components/Dashboard/BodyIWantToTry.js
@@ -4,28 +4,29 @@ import { useState, useContext, useEffect } from "react";
 import { useNavigation } from '@react-navigation/native';
 import { Token, UserData } from '../../context/context';
 import { FlatList, SafeAreaView } from 'react-native';
-import { getPlaces, postPlaces } from '../../../Api/ApiCall';
+import { getPlaces, postPlaces, putPlaces } from '../../../Api/ApiCall';
 import MapView from 'react-native-maps';
 
 export function BodyIWantToTry() {
     const { token, setToken } = useContext(Token);
     const [placesNotGone, setPlacesNotGone] = useState("");
     const [modalVisible, setModalVisible] = useState(false);
+    const [editModalVisible, setEditModalVisible] = useState(false);
     const [title, setTitle] = useState("");
     const [address, setAddress] = useState("");
     const [latitude, setLatitude] = useState("");
     const [longitude, setLongitude] = useState("");
     const [comment, setComment] = useState("");
+    const [dataToEdit, setDataToEdit] = useState("");
+    const [idToEdit, setIdToEdit] = useState("");
 
     useEffect(() => {
         getPlaces(token).then(res => {
             const result = res.data
-            const gone = result.filter((key) => key.attributes.gone)
             const notGone = result.filter((key) => !key.attributes.gone)
-            console.log(gone)
             setPlacesNotGone(notGone)
         })
-    }, []);
+    });
 
     const sendPlace = () => {
         const data = {
@@ -41,6 +42,33 @@ export function BodyIWantToTry() {
         }
         postPlaces(token, data)
         setModalVisible(!modalVisible)
+    }
+
+    const editData = (item) => {
+        setEditModalVisible(true)
+        setDataToEdit(item.attributes)
+        setTitle(item.attributes.title)
+        setComment(item.attributes.comment)
+        setAddress(item.attributes.address)
+        setLongitude(item.attributes.longitude)
+        setLatitude(item.attributes.latitude)
+        setIdToEdit(item.id)
+    }
+
+    const editPlace = () => {
+        const data = {
+            data: {
+                title: title,
+                address: address,
+                comment: comment,
+                latitude: latitude,
+                longitude: longitude,
+                gone: false,
+                type: 1
+            }
+        }
+        putPlaces(token, data, idToEdit)
+        setEditModalVisible(!editModalVisible)
     }
 
     return (
@@ -65,19 +93,30 @@ export function BodyIWantToTry() {
                         }}
                         showsVerticalScrollIndicator={false}
                         renderItem={({ item }) =>
-                            <View>
-                                <Pressable style={{
-                                    width: '85%',
-                                    height: 75,
-                                    flexDirection: 'column',
-                                    borderBottomColor: 'teal',
-                                    borderBottomWidth: 2,
-                                    marginLeft: 15,
-                                }}
-                                >
-                                    <Text style={styles.textTitle}>🍽 {item.attributes.title.toUpperCase()}</Text>
-                                    <Text style={styles.textAddress}>🛩 {item.attributes.address}</Text>
-                                </Pressable>
+                            <View style={styles.list}>
+                                <View>
+                                    <Pressable style={{
+                                        height: 75,
+                                        flexDirection: 'column',
+                                        marginLeft: 15,
+                                    }}
+                                        onPress={() => editData(item)}
+                                    >
+                                        <Text style={styles.textTitle}>🍽 {item.attributes.title.toUpperCase()}</Text>
+                                        <Text style={styles.textAddress}>🛩 {item.attributes.address}</Text>
+                                    </Pressable>
+                                </View>
+                                <View style={styles.editIcon}>
+                                    <Pressable style={{
+                                        height: 75,
+                                        flexDirection: 'column',
+                                        marginLeft: 15,
+                                    }}
+                                        onPress={() => editData(item)}
+                                    >
+                                        <Text style={styles.textIcon}>✏</Text>
+                                    </Pressable>
+                                </View>
                             </View>
                         }
                         keyExtractor={item => item.id}
@@ -130,8 +169,65 @@ export function BodyIWantToTry() {
                                     onChangeText={(longitude) => setLongitude(longitude)}
                                 />
                                 <View style={styles.containerButton}>
-                                    <Pressable style={styles.button} onPress={() => sendPlace()}>
-                                        <Text style={styles.textButton}>+</Text>
+                                    <Pressable style={styles.modalButton} onPress={() => sendPlace()}>
+                                        <Text style={styles.textButton}>Add</Text>
+                                    </Pressable>
+                                </View>
+                            </View>
+
+                        </View>
+                    </View>
+                </Modal>
+
+            </View>
+
+            <View style={styles.centeredView}>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={editModalVisible}
+                    onRequestClose={() => {
+                        setEditModalVisible(!editModalVisible);
+                    }}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <View style={styles.buttonClose}>
+                                <Pressable
+                                    onPress={() => setEditModalVisible(!editModalVisible)}>
+                                    <Text style={styles.textButtonClose}>✖</Text>
+                                </Pressable>
+                            </View>
+                            <View>
+                                <Text style={styles.modalTitle}>Edit a restaurant</Text>
+                                <TextInput
+                                    style={styles.inputModal}
+                                    placeholder={dataToEdit.title}
+                                    onChangeText={(title) => setTitle(title)}
+                                />
+                                <TextInput
+                                    style={styles.inputModal}
+                                    placeholder={dataToEdit.address}
+                                    onChangeText={(address) => setAddress(address)}
+                                />
+                                <TextInput
+                                    style={styles.inputModal}
+                                    placeholder={dataToEdit.comment}
+                                    onChangeText={(comment) => setComment(comment)}
+                                />
+                                <TextInput
+                                    style={styles.inputModal}
+                                    placeholder={"" + dataToEdit.latitude}
+                                    onChangeText={(latitude) => setLatitude(latitude)}
+                                />
+                                <TextInput
+                                    style={styles.inputModal}
+                                    placeholder={"" + dataToEdit.longitude}
+                                    onChangeText={(longitude) => setLongitude(longitude)}
+                                />
+                                <View style={styles.containerButton}>
+                                    <Pressable style={styles.modalButton} onPress={() => editPlace()}>
+                                        <Text style={styles.textButton}>Edit</Text>
                                     </Pressable>
                                 </View>
                             </View>
